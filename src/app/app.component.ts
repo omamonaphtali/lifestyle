8e1e1f6d90f7888fa79a849628c1fed163f30cc0
omamonaphtali <omamonaphtali@gmail.com>
2018-08-26 12:54:09 +0300

    added the gallery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   navbarOpen = false;
   menOpen = false;
   womenOpen = false;
