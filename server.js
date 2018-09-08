c35202cbd929147f3288ce178e365014418422de
omamonaphtali <omamonaphtali@gmail.com>
2018-09-08 21:24:55 +0300

    making it better

--- server.js
+++ server.js
@@ -2,19 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// const forceSSL = function() {
-//   return (req, res, next) => {
-//     if (req.headers['x-forwarded-proto'] !== 'https') {
-//       return res.redirect(
-//        ['https://', req.get('Host'), req.url].join('')
-//       );
-//     }
-//     next();
-//   }
-// }
-
-// app.use(forceSSL());
-
 app.use(express.static('./dist/ng-lifestyle'));
 
 app.get('/*', (req, res) => {
